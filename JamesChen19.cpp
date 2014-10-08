e03cbb68363623c692d9707060e226a629c88550
PavPS <ipavps@gmail.com>
2014-10-09 02:07:36 +0400

    JamesChen19 solved

--- JamesChen19.cpp
+++ JamesChen19.cpp
@@ -24,23 +24,42 @@ Hence ans= NO
 #include <iostream>
 #include <algorithm>
 #include <iomanip>
+#include <bitset>
 
 using namespace std;
 
+bool isZeroSum(vector<int> values)
+{
+	if (values.size() == 1)
+		return values[0] == 0;
+
+	const auto tail = values.back();
+	values.pop_back();
+
+	vector<int> part1(values), part2(values);
+	part1[0] += tail;
+	part2[0] -= tail;
+
+	return isZeroSum(part1) || isZeroSum(part2);
+}
+
 void JamesChen19()
 {
-	const auto ArraySize = 10;
+	const auto ArraySize = 3;
 	cout << "        ";
 	vector<int> values;
 	for (unsigned i = 0; i < ArraySize; ++i)
 	{
-		values.push_back(i);
+		values.push_back(rand() % 10);
 		cout << "#" << setfill('0') << setw(2) << i << " ";
 	}
 	cout << endl;
 
-	random_shuffle(values.begin(), values.end());
+	values = { 3, 6, 2 };
 
 	cout.fill(' ');
 	cout << "values: "; for (const auto& v : values)cout << " " << setw(2) << v << " "; cout << endl;
+
+	const auto possible = isZeroSum(values);
+	cout << "Possible 0: " << (possible ? "TRUE" : "FALSE") << endl;
 }
