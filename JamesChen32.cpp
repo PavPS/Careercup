bdc9f3463519e047ac166179005e18d346ce8e8d
PavPS <ipavps@gmail.com>
2014-10-12 16:36:44 +0400

    JamesChen32 complete

--- JamesChen32.cpp
+++ JamesChen32.cpp
@@ -1,7 +1,7 @@
 #include "stdafx.h"
 
 /*
-
+Design an efficient algorithm for computing c(n, k) that has the property that it never overflows if c(n, k) can be represented as an integer assume n and k are both integers.
 */
 
 #include <iostream>
@@ -12,7 +12,20 @@
 
 using namespace std;
 
+// See pascal triangle. How you extract subb from upper row (n-1) and two neighbors (k and k-1) 
+
+int conputeNK(int n, int k)
+{
+	if (n == k || k == 0)
+		return 1;
+
+	return conputeNK(n - 1, k) + conputeNK(n - 1, k - 1);
+}
+
 void JamesChen32()
 {
+	const int n = 5;
+	const int k = 2;
 
+	cout << "Result: " << conputeNK(n, k) << endl;
 }
