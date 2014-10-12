f0bb063cf72f4969a24a36056faa15d2b9b44cab
PavPS <ipavps@gmail.com>
2014-10-12 16:07:02 +0400

    31 complete

--- JamesChen31.cpp
+++ JamesChen31.cpp
@@ -1,7 +1,7 @@
 #include "stdafx.h"
 
 /*
-
+Calculate (x^y)%z without pow()
 */
 
 #include <iostream>
@@ -12,6 +12,30 @@
 
 using namespace std;
 
+int calcPowRecursion(int x, int y)
+{
+	if (y == 0)
+		return 1;
+
+	return x * calcPowRecursion(x, y - 1);
+}
+
+int calcMod(int value, int z)
+{
+	if (value < z)
+		return value;
+
+	return calcMod(value - z, z);
+}
+
 void JamesChen31()
 {
+	const int x = 3;
+	const int y = 3;
+	const int z = 10;
+
+	const int p = calcPowRecursion(x, y);
+	const auto result = calcMod(p, z);
+
+	cout << "Result: " << result << endl;
 }
