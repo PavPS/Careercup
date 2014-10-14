3eaaeb329d2ce708da889dda56c6964603603451
Pavel Pavlov <donot@mail.me>
2014-10-14 14:55:59 +0200

    JamesChen49.cpp complete

--- JamesChen49.cpp
+++ JamesChen49.cpp
@@ -3,7 +3,8 @@
 /*
 ##Pretty much interested question asked in AMAZON Written test##
 
-Take a string, store it as digits; str="678876". Now Check if the given str return TWO EQUAL PARTS with equal sum (6+7+8=8+7+6);if it is correct then replace the str(678876) in the new string, str1="12345876678"
+Take a string, store it as digits; str="678876". Now Check if the given str return TWO EQUAL PARTS with equal sum (6+7+8=8+7+6);
+if it is correct then replace the str(678876) in the new string, str1="12345876678"
 
 Final output should be: "12345678876"
 I tried this ..but the interviewer didnt satisfy..
@@ -19,7 +20,27 @@ using namespace std;
 
 void JamesChen49()
 {
-	const string movie = "up";
+	const string str = "678876";
+
+	unsigned left = 0, right = str.size();
+	int sum = 0;
+
+	while(left != right)
+	{
+		if (sum <= 0)
+		{
+			sum += (int)str[left++];
+		}
+		else if (sum > 0)
+		{
+			sum -= (int)str[--right];
+		}
+	}
+
+	if (!sum)
+		cout << "12345" + str << endl;
+	else
+		cout << "SORRY" << endl;
 }
 
 #if !defined(_MSC_VER)
